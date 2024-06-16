55b5f54ed6560889fc2c70d8e7c850093a38dba7
Martí Espinosa <martiespinosa99@gmail.com>
2024-06-16 20:33:59 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Martí Espinosa Farran on 16/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
